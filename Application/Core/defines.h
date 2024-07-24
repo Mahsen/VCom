ecbe762895fe4d79ae20a0b49c1cc1db3693baa0
Mahsen <not.select@gmail.com>
2024-07-24 08:28:21 +0330

    Create project

---
+++ Application/Core/defines.h
@@ -0,0 +1,72 @@
+#ifndef __DEFINES_H
+#define __DEFINES_H
+//----------------------------------------------------------
+#include "cmsis_os2.h"
+#include "rtx_os.h"
+#include "string.h"
+#include <string.h>
+#include <stdio.h>
+#include "stm32f10x.h"
+//----------------------------------------------------------
+#define TASK_MANAGER_OS_TICK                                 1000  //OS_TICK in "RTX_Conf_CM.c" 
+#define OneUnic                                              1
+#define MSec   				    									                 *1000/TASK_MANAGER_OS_TICK
+#define Sec                         	    	                 *1000000/TASK_MANAGER_OS_TICK
+
+#define OFFSETOF(Type, Field)                               ((unsigned long) &(((Type *) 0)->Field))
+//----------------------------------------------------------
+typedef void (*CallBack_Type)(void);
+//----------------------------------------------------------
+#define     C_UNIC              * 1
+//----------------------------------------------------------
+typedef signed char     S8;
+typedef unsigned char   U8;
+typedef short           S16;
+typedef unsigned short  U16;
+typedef int             S32;
+typedef unsigned int    U32;
+typedef long long       S64;
+typedef unsigned long long U64;
+typedef unsigned char   BIT;
+typedef unsigned int    BOOL;
+//----------------------------------------------------------
+#define     NULL_CHAR           0x00
+#define     CR_CHAR             0x0D
+#define     LF_CHAR             0x0A
+#define     ACK_CHAR            0x06
+#define     CTRLZ_CHAR          0x1A
+#define     DEL_CHAR            0x7F
+#define     SOH_CHAR            0x01
+#define     STX_CHAR            0x02
+#define     ETX_CHAR            0x03
+#define     EOT_CHAR            0x04
+#define     NAK_CHAR            0x15
+//----------------------------------------------------------
+//	#ifndef TRUE
+//		#define   TRUE               1
+//	#endif
+//	#ifndef TRUE
+//		#define   FALSE              0
+//	#endif
+//	#ifndef SET
+//		#define   SET                1
+//	#endif
+//	#ifndef RESET
+//		#define   RESET              0
+//	#endif
+	#ifndef OUTPUT
+		#define   OUTPUT             1
+	#endif
+	#ifndef INPUT
+		#define   INPUT              0
+	#endif
+//	#ifndef ENABLE
+//		#define   ENABLE             1
+//	#endif
+//	#ifndef DISABLE
+//		#define   DISABLE            0
+//	#endif
+//	#ifndef UP
+//		#define   UP                 1
+//	#endif
+#endif
---
+++ Application/RTE/_Main/RTE_Components.h
@@ -0,0 +1,41 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'main' 
+ * Target:  'Main' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.2 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+/* Keil.MDK-Plus::USB:CORE:Release:6.14.1 */
+#define RTE_USB_Core                    /* USB Core */
+          #define RTE_USB_Core_Release            /* USB Core Release Version */
+/* Keil.MDK-Plus::USB:Device:6.14.1 */
+#define RTE_USB_Device_0                /* USB Device 0 */
+
+/* Keil.MDK-Plus::USB:Device:CDC:6.14.1 */
+#define RTE_USB_Device_CDC_0            /* USB Device CDC instance 0 */
+
+/* Keil::CMSIS Driver:USB Device:2.2 */
+#define RTE_Drivers_USBD0               /* Driver USBD0 */
+/* Keil::Device:StdPeriph Drivers:Framework:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+/* Keil::Device:StdPeriph Drivers:GPIO:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_GPIO
+/* Keil::Device:StdPeriph Drivers:RCC:3.6.0 */
+#define RTE_DEVICE_STDPERIPH_RCC
+
+
+#endif /* RTE_COMPONENTS_H */
